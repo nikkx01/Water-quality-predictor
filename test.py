381e28a23719185a45b5b6c8ee336dc4eeeac9fe
Nikhil Anand <nikhildfire8102@gmail.com>
2026-04-27 16:36:32 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from utils.preprocess import load_data, clean_data, split_data
-
-df = load_data()
-df = clean_data(df)
-
-X, y = split_data(df)
-
-print("X shape:", X.shape)
-print("y shape:", y.shape)
